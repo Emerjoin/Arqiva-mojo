d56740c0d7a9c77ab7879909e4926ed88e154d3b
mjunior <francisco.junior.mario@gmail.com>
2017-03-17 10:28:36 +0200

    run servlet created

--- src/main/java/org/emerjoin/arqiva/mojo/ArqivaRunServlet.java
+++ src/main/java/org/emerjoin/arqiva/mojo/ArqivaRunServlet.java
@@ -1,10 +1,42 @@
 package org.emerjoin.arqiva.mojo;
 
+import org.emerjoin.arqiva.core.Project;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author Mário Júnior
  */
-public class ArqivaRunServlet {
+public class ArqivaRunServlet extends HttpServlet {
 
+    private boolean invalidateTopicsTree = false;
+    private Project arqivaProject = null;
+
+    public ArqivaRunServlet(Project arqivaProject, boolean invalidateTopicsTree){
+
+        this.arqivaProject = arqivaProject;
+        this.invalidateTopicsTree = invalidateTopicsTree;
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        if(invalidateTopicsTree)
+            arqivaProject.invalidateTopicsTree();
+
+
+        //TODO: Render page here
+
+
+
+    }
 
 
 }
