34fe666b3a74ed1aabcc421c54ea1dac54bcf7b9
mjunior <francisco.junior.mario@gmail.com>
2017-03-18 14:23:13 +0200

    ArqivaBuildMojo implementation began

--- src/main/java/org/emerjoin/arqiva/mojo/ArqivaBuildMojo.java
+++ src/main/java/org/emerjoin/arqiva/mojo/ArqivaBuildMojo.java
@@ -3,6 +3,8 @@ package org.emerjoin.arqiva.mojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.emerjoin.arqiva.core.Arqiva;
+import org.emerjoin.arqiva.core.Project;
 
 /**
  * @author Mário Júnior
@@ -10,14 +12,23 @@ import org.apache.maven.plugins.annotations.Parameter;
 @Mojo(name ="build")
 public class ArqivaBuildMojo extends AbstractArqivaMojo {
 
-
-
     @Parameter(defaultValue = "")
     private String projectBuilderName;
 
     public void execute() throws MojoExecutionException
     {
-        getLog().info( "Hello, world." );
+
+        if(projectBuilderName.equals(""))
+            getLog().info("Building Arqiva project with default builder");
+        else
+            getLog().info("Building Arqiva project with "+projectBuilderName+" builder");
+
+        Project arqivaProject = createProject();
+        Arqiva arqiva = new Arqiva(arqivaProject);
+        arqiva.buildProject(projectBuilderName);
+
+        getLog().info("Arqiva project build succeeded!");
+
     }
 
 
